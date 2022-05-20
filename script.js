4ca5ca3ab83a2d024e8867f0a536a28caae93865
saskiabt <sskbinder@gmail.com>
2022-05-20 13:10:56 -0400

    calculator can now loop through evaluate several times using the same operator, and print the result of each evaluate function to the display, saving the evaluate() result as the new firstNumber

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Global Variables 
-let firstNumber;
-let secondNumber;
-let operation;
+let firstNumber = '';
+let secondNumber = '';
+let operation = '';
 let newResult = true; 
 
 // Math Functions 
@@ -15,7 +15,7 @@ const add = function(num1,num2) {
 const subtract = function(num1,num2) {
     num1 = parseFloat(num1); 
     num2 = parseFloat(num2); 
-    return num1 + num2; 
+    return num1 - num2; 
 }
 
 const multiply = function(num1,num2) { 
@@ -31,14 +31,9 @@ const divide = function(num1,num2) {
     if (num2 === 0) { 
      return 'ERROR'; 
     } 
-    
     return num1 / num2; 
 }
 
-const evaluate = function(operator,...params) { 
-    let result = operator(...params); 
-    return result; 
-}
 
 // Clear display function and clear button
 function clearDisplay() {
@@ -59,8 +54,8 @@ clearButton.addEventListener('click', () => {
 
 //populate the display with NUM if display is cleared, or else clear display and populate display with num
 function populateDisplay(num){ 
-
     const displayValue = document.getElementById('numbers-display'); 
+
     if (!newResult) { 
         displayNumbers(); 
     } else {
@@ -77,9 +72,9 @@ function populateDisplay(num){
             displayValue.textContent = num; 
         } else { 
             displayValue.textContent += num;
-        }
-    }
-}
+        };
+    };
+};
 
 // On click of each number buttons, populate the display with numberButton textContent
 
@@ -103,7 +98,139 @@ let numberButtons = document.querySelectorAll('.numberButton');
 })
 
 // Math buttons 
-const multiplyBtn = document.getElementById("multiply-btn"); 
+const mathButtons = document.querySelectorAll('.math-buttons'); 
+[...mathButtons].forEach((button) => {
+    button.addEventListener('click', function(event) {
+        let target = event.target
+
+        if(target.id === "add-btn") { 
+            const displayValue = document.getElementById('numbers-display'); 
+            if (!firstNumber) { // if firstNumber is empty
+                firstNumber = parseFloat(displayValue.textContent); 
+                console.log(`FirstNumber is ${firstNumber}`) 
+                operation = add; 
+                clearDisplay(); 
+            } else { // if firstNumber has already been declared 
+                if (operation === multiply || operation === divide || operation === add || operation === subtract) {  // runs evaluate if first set of params has already been chosen
+                    operation = add;
+                    evaluate(); 
+                    console.log(newResult)
+                } else { // if firstNumber has been added because user input first number and pressed equals without adding an operator
+                    firstNumber = parseFloat(displayValue.textContent); 
+                    operation = add; 
+                    clearDisplay(); 
+                }
+            }
+        }
+
+        if(target.id === "subtract-btn") { 
+            const displayValue = document.getElementById('numbers-display'); 
+            if (!firstNumber) { // if firstNumber is empty
+                firstNumber = parseFloat(displayValue.textContent); 
+                console.log(`FirstNumber is ${firstNumber}`) 
+                operation = subtract; 
+                clearDisplay(); 
+            } else { // if firstNumber has already been declared 
+                if (operation === multiply || operation === divide || operation === add || operation === subtract) {  // runs evaluate if first set of params has already been chosen
+                    operation = subtract
+                    evaluate(); 
+                } else { // if firstNumber has been added because user input first number and pressed equals without adding an operator
+                    firstNumber = parseFloat(displayValue.textContent); 
+                    operation = subtract; 
+                    clearDisplay(); 
+                }
+            }
+        }
+
+        if(target.id === "multiply-btn") { 
+            const displayValue = document.getElementById('numbers-display'); 
+            if (!firstNumber) { // if firstNumber is empty
+                firstNumber = parseFloat(displayValue.textContent); 
+                console.log(`FirstNumber is ${firstNumber}`) 
+                operation = multiply; 
+                clearDisplay(); 
+            } else { // if firstNumber has already been declared 
+                if (operation === multiply || operation === divide || operation === add || operation === subtract) {  // runs evaluate if first set of params has already been chosen
+                    evaluate(); 
+                } else { // if firstNumber has been added because user input first number and pressed equals without adding an operator
+                    firstNumber = parseFloat(displayValue.textContent); 
+                    operation = multiply; 
+                    clearDisplay(); 
+                }
+            }
+        }
+
+        if(target.id === "divide-btn") { 
+            const displayValue = document.getElementById('numbers-display'); 
+            if (!firstNumber) { // if firstNumber is empty
+                firstNumber = parseFloat(displayValue.textContent); 
+                console.log(`FirstNumber is ${firstNumber}`) 
+                operation = divide; 
+                clearDisplay(); 
+            } else { // if firstNumber has already been declared 
+                if (operation === multiply || operation === divide || operation === add || operation === subtract) {  // runs evaluate if first set of params has already been chosen
+                    evaluate(); 
+                } else { // if firstNumber has been added because user input first number and pressed equals without adding an operator
+                    firstNumber = parseFloat(displayValue.textContent); 
+                    operation = divide; 
+                    clearDisplay(); 
+                }
+            }
+        }
+
+
+    })
+})
+
+const operate = function(...args) { 
+    let operator = args[0]; 
+    let nums = []; 
+
+    for(let i=1; i<args.length; i++) { 
+        nums[i-1] = args[i]
+    }
+    return operator(...nums)
+}; 
+
+function evaluate() { 
+    const displayValue = document.getElementById('numbers-display'); 
+
+    if(!operation) { // if an operation has not been chosen and user selects equals before selecting operation
+        firstNumber = parseFloat(displayValue.textContent); 
+        newResult = true; 
+    } else if (typeof firstNumber !== 'number') { // if firstNumber has been reset to blank string, allows for looping through multiple calls of evaluate()
+        firstNumber = parseFloat(displayValue.textContent); 
+        let result = operate(operation, firstNumber,secondNumber); 
+        clearDisplay(); 
+        populateDisplay(result); 
+        firstNumber = ''; 
+        newResult = true;
+    } else { 
+        secondNumber = firstNumber; 
+        console.log(secondNumber); 
+        firstNumber = displayValue.textContent; // sets firstNumber equal to the result
+        console.log(firstNumber); 
+
+        let result = operate(operation, firstNumber,secondNumber); 
+        clearDisplay(); 
+        populateDisplay(result); 
+
+        secondNumber= result;
+        firstNumber = ''
+        newResult = true; 
+    }
+}
+
+const equalsButton = document.getElementById("equal-btn")
+equalsButton.addEventListener('click', () => { 
+   if (!firstNumber) { 
+    const displayValue = document.getElementById('numbers-display'); 
+    firstNumber = parseFloat(displayValue.textContent)
+   } else { 
+       console.log(operation); 
+       evaluate(); 
+   }
+})
 // const addBtn
 // const subtractBtn
 // const divideBtn
